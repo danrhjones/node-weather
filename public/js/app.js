033c5569c728b39256b153cc67c4ad0783a1bf0c
Dan Jones <dan.rh.jones@gmail.com>
2019-12-13 17:01:18 +0000

    updating somethings to get this working on Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,9 +8,10 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
 
+
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
-  const url = "http://localhost:3000/weather?address=" + location;
+  const url = "/weather?address=" + location;
 
   fetch(url).then((response) => {
     response.json().then((data) => {
